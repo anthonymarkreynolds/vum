8daa8d73608eb147641c5298940749f9c4bcdea3
anthonymarkreynolds <anthonymarkreynolds@outlook.com>
2022-07-19 19:47:07 +1000

    add cursor wrap

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
+var cursor = {
+    pos: { x: 0, y: 0 },
+    char: '_'
+};
 var windowProps = {
     mode: 'normal',
     width: 0,
     height: 0,
-    textCanvas: ''
+    textCanvas: '',
+    pendingRedraw: true
+};
+var sliceAt = function (textCanvas, text, pos) {
+    var i = (windowProps.width + 1) * pos.y + pos.x;
+    return textCanvas.slice(0, i) + text + textCanvas.slice(i + text.length, -1);
 };
 // const draw = (textObject:string, textCanvas:TextCanvas, location:CoOrd): TextCanvas => ;
 var drawBlank = function () {
@@ -11,7 +20,7 @@ var drawBlank = function () {
 var drawCanvas = function () {
     windowProps.textCanvas = drawBlank();
     var view = document.querySelector('#view');
-    view.innerText = windowProps.textCanvas;
+    view.innerText = sliceAt(windowProps.textCanvas, cursor.char, cursor.pos);
     console.log(windowProps);
 };
 var handleResize = function () {
@@ -24,21 +33,44 @@ var handleResize = function () {
         console.log('resizing');
     }
 };
+var runAction = function (action, updateUi) {
+    action();
+    if (updateUi)
+        drawCanvas();
+    console.log(updateUi);
+};
+var nKeyActions = {
+    j: function () { cursor.pos.y = (cursor.pos.y + 1) % windowProps.height; },
+    k: function () { cursor.pos.y = (cursor.pos.y - 1 + windowProps.height) % windowProps.height; },
+    l: function () { cursor.pos.x = (cursor.pos.x + 1) % (windowProps.width); },
+    h: function () { cursor.pos.x = (cursor.pos.x - 1 + windowProps.width) % (windowProps.width); }
+};
+var handleNormalKey = function (key) {
+    runAction(nKeyActions[key], true);
+};
 var handleKeypress = function (event) {
-    switch (windowProps.mode) {
-        case 'normal':
-            console.log(event.key);
-            break;
-        case 'insert':
-            break;
-        case 'select':
-            break;
-        case 'command':
-            break;
-        default:
-            windowProps.mode = 'normal';
+    if (event.key === 'Escape') {
+        windowProps.mode = 'normal';
+    }
+    else {
+        switch (windowProps.mode) {
+            case 'normal':
+                console.log(event.key);
+                handleNormalKey(event.key);
+                console.log(cursor.pos);
+                break;
+            case 'insert':
+                break;
+            case 'select':
+                break;
+            case 'command':
+                break;
+            default:
+                windowProps.mode = 'normal';
+        }
     }
 };
 // const view = document.body
 window.addEventListener('keydown', handleKeypress);
 window.addEventListener('resize', function () { return handleResize(); });
+handleResize();
--- index.ts
+++ index.ts
@@ -1,34 +1,51 @@
 type Mode = 'normal' | 'insert' | 'select' | 'command'
 type TextCanvas = string
+type Point = number
 
 interface WindowProps {
   mode: Mode
   width: number
   height: number
   textCanvas: TextCanvas
+  pendingRedraw: boolean
 }
 
-interface CoOrd {
-  x: number,
-  y: number
+interface Coord {
+  x: Point,
+  y: Point
+}
+
+interface Cursor {
+  pos: Coord,
+  char: string
+}
+
+const cursor: Cursor = {
+  pos: { x: 0, y: 0 },
+  char: '_'
 }
 
 const windowProps: WindowProps = {
   mode: 'normal',
   width: 0,
   height: 0,
-  textCanvas: ''
+  textCanvas: '',
+  pendingRedraw: true
+}
+
+const sliceAt = (textCanvas:TextCanvas, text:string, pos:Coord): TextCanvas => {
+  const i = (windowProps.width + 1) * pos.y + pos.x
+  return textCanvas.slice(0, i) + text + textCanvas.slice(i + text.length, -1)
 }
 
 // const draw = (textObject:string, textCanvas:TextCanvas, location:CoOrd): TextCanvas => ;
-
 const drawBlank = ():string => {
   return ('·'.repeat(windowProps.width) + '\n').repeat(windowProps.height)
 }
 const drawCanvas = (): void => {
   windowProps.textCanvas = drawBlank()
   const view = document.querySelector('#view') as HTMLElement
-  view.innerText = windowProps.textCanvas
+  view.innerText = sliceAt(windowProps.textCanvas, cursor.char, cursor.pos)
   console.log(windowProps)
 }
 
@@ -43,22 +60,51 @@ const handleResize = (): void => {
   }
 }
 
+const runAction = (action:Function, updateUi:boolean): void => {
+  action()
+  if (updateUi) drawCanvas()
+  console.log(updateUi)
+}
+
+interface KeyActions {
+  [key: string]: Function
+}
+
+const nKeyActions: KeyActions = {
+  j: () => { cursor.pos.y = (cursor.pos.y + 1) % windowProps.height },
+  k: () => { cursor.pos.y = (cursor.pos.y - 1 + windowProps.height) % windowProps.height },
+  l: () => { cursor.pos.x = (cursor.pos.x + 1) % (windowProps.width) },
+  h: () => { cursor.pos.x = (cursor.pos.x - 1 + windowProps.width) % (windowProps.width) }
+}
+
+const handleNormalKey = (key:string): void => {
+  runAction(nKeyActions[key], true)
+}
+
 const handleKeypress = (event: KeyboardEvent): void => {
-  switch (windowProps.mode) {
-    case 'normal':
-      console.log(event.key)
-      break
-    case 'insert':
-      break
-    case 'select':
-      break
-    case 'command':
-      break
-    default:
-      windowProps.mode = 'normal'
+  if (event.key === 'Escape') {
+    windowProps.mode = 'normal'
+  } else {
+    switch (windowProps.mode) {
+      case 'normal':
+        console.log(event.key)
+        handleNormalKey(event.key)
+        console.log(cursor.pos)
+        break
+      case 'insert':
+        break
+      case 'select':
+        break
+      case 'command':
+        break
+      default:
+        windowProps.mode = 'normal'
+    }
   }
 }
 
 // const view = document.body
 window.addEventListener('keydown', handleKeypress)
 window.addEventListener('resize', () => handleResize())
+
+handleResize()
