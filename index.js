d482b9dacdbe60dec8a212082bb71ea9b07bcc61
anthonymarkreynolds <anthonymarkreynolds@outlook.com>
2022-07-19 17:54:01 +1000

    migrate to typescript

--- index.js
+++ index.js
@@ -6,24 +6,39 @@ var windowProps = {
 };
 // const draw = (textObject:string, textCanvas:TextCanvas, location:CoOrd): TextCanvas => ;
 var drawBlank = function () {
-    return 'test'.repeat(3);
-    // return ('*'.repeat(windowProps.width) + '\n')//.repeat(windowProps.height).slice(0, -2)
-    // windowProps.textCanvas = Array.from(
-    //   { length: windowProps.height },
-    //   () => line
-    // ).join('\n')
+    return ('·'.repeat(windowProps.width) + '\n').repeat(windowProps.height);
 };
 var drawCanvas = function () {
-    drawBlank();
+    windowProps.textCanvas = drawBlank();
     var view = document.querySelector('#view');
     view.innerText = windowProps.textCanvas;
+    console.log(windowProps);
 };
 var handleResize = function () {
     var newHeight = Math.floor(document.body.clientHeight / 25);
     var newWidth = Math.floor(document.body.clientWidth / 12.03);
     if ((newWidth !== windowProps.width) || (newHeight !== windowProps.height)) {
+        windowProps.width = newWidth;
+        windowProps.height = newHeight;
         drawCanvas();
+        console.log('resizing');
     }
 };
-window.addEventListener('keydown', console.log);
+var handleKeypress = function (event) {
+    switch (windowProps.mode) {
+        case 'normal':
+            console.log(event.key);
+            break;
+        case 'insert':
+            break;
+        case 'select':
+            break;
+        case 'command':
+            break;
+        default:
+            windowProps.mode = 'normal';
+    }
+};
+// const view = document.body
+window.addEventListener('keydown', handleKeypress);
 window.addEventListener('resize', function () { return handleResize(); });
--- index.ts
+++ index.ts
@@ -23,26 +23,42 @@ const windowProps: WindowProps = {
 // const draw = (textObject:string, textCanvas:TextCanvas, location:CoOrd): TextCanvas => ;
 
 const drawBlank = ():string => {
-  return 'test'.repeat(3)
-  // return ('*'.repeat(windowProps.width) + '\n')//.repeat(windowProps.height).slice(0, -2)
-  // windowProps.textCanvas = Array.from(
-  //   { length: windowProps.height },
-  //   () => line
-  // ).join('\n')
+  return ('·'.repeat(windowProps.width) + '\n').repeat(windowProps.height)
 }
 const drawCanvas = (): void => {
-  drawBlank()
+  windowProps.textCanvas = drawBlank()
   const view = document.querySelector('#view') as HTMLElement
   view.innerText = windowProps.textCanvas
+  console.log(windowProps)
 }
 
 const handleResize = (): void => {
   const newHeight:number = Math.floor(document.body.clientHeight / 25)
   const newWidth:number = Math.floor(document.body.clientWidth / 12.03)
   if ((newWidth !== windowProps.width) || (newHeight !== windowProps.height)) {
+    windowProps.width = newWidth
+    windowProps.height = newHeight
     drawCanvas()
+    console.log('resizing')
   }
 }
 
-window.addEventListener('keydown', console.log)
+const handleKeypress = (event: KeyboardEvent): void => {
+  switch (windowProps.mode) {
+    case 'normal':
+      console.log(event.key)
+      break
+    case 'insert':
+      break
+    case 'select':
+      break
+    case 'command':
+      break
+    default:
+      windowProps.mode = 'normal'
+  }
+}
+
+// const view = document.body
+window.addEventListener('keydown', handleKeypress)
 window.addEventListener('resize', () => handleResize())
